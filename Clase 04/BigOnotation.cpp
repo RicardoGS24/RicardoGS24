45776882f81873960a24c1cb8380f67099786f82
RicardoGS24 <A00835912@tec.mx>
2023-08-17 23:32:26 -0600

    Actividad 1.3

--- Clase 04/BigOnotation.cpp
+++ Clase 04/BigOnotation.cpp
@@ -3,33 +3,37 @@
 using namespace std;
 
 // Función iterativa
+// De orden: O(n)
 int returnImpar(vector <int> a){
     int suma = 0;
-    for(int i =0; i<a.size(); i++){ // O(n)
-        if (a[i] % 2 != 0){  //
+    for(int i =0; i<a.size(); i++){
+        if (a[i] % 2 != 0){  
             suma += a[i];
         }
     }
     return suma;
 }
 
-int returnImparRecur(vector <int> a){
-    int i = 0;
-    if (a[0] % 2 != 0){
-        return a[0] + returnImparRecur();
+// Función recursiva
+// De orden: O(n)
+int returnImparRecur(vector <int> a,int b){
+    if (b < a.size()){
+        if (a[b] % 2 != 0){
+            return a[b] + returnImparRecur(a,b+1);
+        }
+        else{
+            return returnImparRecur(a,b+1);
+        }
     }
-    else{
-        return returnImparRecur();
-    }
+    return 0;
 }
-
-
 
 int main()
 {
-    vector <int> intVector = {3,3,3,3,3};
+    vector <int> intVector = {1,2,3,4,5,6,7,8,9};
     // Suma iterativa
-    cout << "La suma de números impares es: " << returnImpar(intVector) << endl;
-
+    cout << "La suma iterativa de números impares es: " << returnImpar(intVector) << endl;
+    // Suma Recursiva
+    cout << "La suma recursiva de números impares es: " << returnImparRecur(intVector,0) << endl;
     return 0;
 }
